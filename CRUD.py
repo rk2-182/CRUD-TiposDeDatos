cbd0c1ddf5c73ace73a65c0264192fbd56f652b4
rk2-182 <ricardoplus@live.cl>
2021-12-03 09:54:03 -0300

    Se crea archivo independiete para crud y menu

---
+++ CRUD.py
@@ -0,0 +1,74 @@
+import db #importamos archivo con la conexion
+import datetime
+import os
+
+#creamos una instancia del archivo que contiene la conexion y su funcion
+connect = db.conexion()
+
+#valores retornados de la funcion
+bd = connect[0]
+cursor = connect[1]
+
+
+#Funcion para mostrar datos de las tablas
+def mostrarDatos(opcion):
+    if opcion == 1:
+        cursor.execute("select * from proveedor")
+        resultado = cursor.fetchall()
+        os.system('cls')
+        print("Tabla Proveedor")
+        print("---------------")
+        for i in resultado:
+            print(i)
+    elif opcion == 2:
+        cursor.execute("select * from productos")
+        resultado = cursor.fetchall()
+        os.system('cls')
+        print("Tabla Productos")
+        print("---------------")
+        for i in resultado:
+            print(i)
+    elif opcion == 3:
+        cursor.execute("select * from cliente")
+        resultado = cursor.fetchall()
+        os.system('cls')
+        print("Tabla Clientes")
+        print("---------------")
+        for i in resultado:
+            print("Rut:",i[0])
+            print("Nombre:",i[1])
+            print("Fecha Nacimiento: ",i[2])
+            print("Telefono: ",i[3])
+            print("--------------------")
+            print(" ")
+
+#Insertar Datos
+def insertarDatosProveedor():
+    sql = 'insert into proveedor (nombre,direccion) values (%s,%s)'
+    valores = ('losperros banquetera','Napoles N°999')
+    cursor.execute(sql,valores) #recibe 2argumentos: las instrucciones sql y los valores
+    bd.commit()
+
+    if cursor.rowcount == 1:
+        print("Datos ingresados")
+    else:
+        print("Error al ingresar datos")
+
+def insertarDatosCliente():
+    dni=int(input("Ingrese el DNI: "))
+    nombre = input("Ingrese su nombre: ")
+    año=int(input("Ingrese su año de nacimiento: "))
+    mes=int(input("Ingresa tu mes de nacimiento: "))
+    dia=int(input("Ingresa el dia de nacimiento: "))
+    fechaNacimiento = datetime.date(año,mes,dia)
+    telefono = input("Ingrese tu numero telefonico: ")
+
+    sql = 'insert into cliente (dni,nombre,fechaNac,telefono) values (%s,%s,%s,%s)'
+    datos = (dni,nombre,fechaNacimiento,telefono)
+    cursor.execute(sql,datos)
+    bd.commit()
+
+    if cursor.rowcount == 1:
+        print("Datos ingresados")
+    else:
+        print("Error al ingresar datos")
--- main.py
+++ main.py
@@ -1,82 +1,37 @@
+import CRUD
 import os
-import db #importamos archivo con la conexion
-import datetime
-
-#creamos una instancia del archivo que contiene la conexion y su funcion
-connect = db.conexion()
-
-#valores retornados de la funcion
-bd = connect[0]
-cursor = connect[1]
 
 
-#Funion para mostrar datos de las tablas
-def mostrarDatos(opcion):
+
+#*****Menu*****
+def Menu():
+    os.system('cls')
+    print("========Empresa 1.0========\n")
+
+    print("1.-Revisar Datos de la Empresa")
+    print("2.-Ingresar Datos a la Empresa")
+
+    opcion = int(input("Seleccione Una Opción: "))
+
+
     if opcion == 1:
-        cursor.execute("select * from proveedor")
-        resultado = cursor.fetchall()
-        os.system('cls')
-        print("Tabla Proveedor")
-        print("---------------")
-        for i in resultado:
-            print(i)
-    elif opcion == 2:
-        cursor.execute("select * from productos")
-        resultado = cursor.fetchall()
-        os.system('cls')
-        print("Tabla Productos")
-        print("---------------")
-        for i in resultado:
-            print(i)
-
-def insertarDatosProveedor():
-    sql = 'insert into proveedor (nombre,direccion) values (%s,%s)'
-    valores = ('losperros banquetera','Napoles N°999')
-    cursor.execute(sql,valores) #recibe 2argumentos: las instrucciones sql y los valores
-    bd.commit()
-
-    if cursor.rowcount == 1:
-        print("Datos ingresados")
+        print("¿Que datos desea revisar?: ")
+        print("1.-Proveedor")
+        print("2.-Productos")
+        print("3.-Clientes")
+        op = int(input("Ingresa la opción:"))
+        if op==1:
+            CRUD.mostrarDatos(1)
+        elif op==2:
+            CRUD.mostrarDatos(2)
+        elif op ==3:
+            CRUD.mostrarDatos(3)
+        else:
+            print("Opcion erronea")
     else:
-        print("Error al ingresar datos")
-
-def insertarDatosCliente():
-    dni=int(input("Ingrese el DNI: "))
-    nombre = input("Ingrese su nombre: ")
-    año=int(input("Ingrese su año de nacimiento: "))
-    mes=int(input("Ingresa tu mes de nacimiento: "))
-    dia=int(input("Ingresa el dia de nacimiento: "))
-    fechaNac = datetime.date(año,mes,dia)
-    telefono = input("Ingrese tu numero telefonico: ")
-
-    sql = 'insert into cliente (dni,nombre,fechaNac,telefono) values (%s,%s,%s,%s)'
-    datos = (dni,nombre,fechaNac,telefono)
-    cursor.execute(sql,datos)
-    bd.commit()
-
-    if cursor.rowcount == 1:
-        print("Datos ingresados")
-    else:
-        print("Error al ingresar datos")
+        print("Opcion erronea")
 
 
-
-
-print("----------Empresa 1.0----------\n")
-print("1.-Proveedor")
-print("2.-Productos")
-opcion=int(input("¿Que datos desea revisar?: "))
-
-if opcion==1:
-    mostrarDatos(1)
-elif opcion==2:
-    mostrarDatos(2)
-else:
-    print("Opcion erronea")
-
-
-print("Ingresando datos cliente")
-insertarDatosCliente()
-
+Menu()
 
 
